c5bb941fa141e2eaf0428824c54c1b0e6f3c0ebc
haeun0228 <eileen0228@naver.com>
2025-03-06 12:21:34 -0500

    Add nbo.cpp

---
+++ main.h
@@ -0,0 +1,7 @@
+#pragma once 
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+uint32_t ntohl(uint32_t n);
---
+++ nbo.cpp
@@ -0,0 +1,10 @@
+//sum.cpp 
+#include "main.h"
+
+uint32_t ntohl(uint32_t n){
+	uint32_t n1 = (n & 0x000000FF) << 24;
+	uint32_t n2 = (n & 0x0000FF00) << 8;
+	uint32_t n3 = (n & 0x00FF0000) >> 8;
+	uint32_t n4 = (n & 0xFF000000) >> 24; 
+	return n1 | n2 | n3 | n4;
+}
